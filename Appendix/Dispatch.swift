69ba2759996250d94fbbd519ea0dc8fa49291779
wess <wcope@me.com>
2015-10-29 12:24:11 -0400

    Adds Dispatch once

--- Appendix/Dispatch.swift
+++ Appendix/Dispatch.swift
@@ -10,6 +10,20 @@ import Foundation
 
 public class Dispatch : NSObject {
     /**
+         Executes block once
+         
+         :param: Block to execute once
+     */
+     
+    public class func executeOnce(block:()->Void) {
+        struct Static {
+            static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token, block)
+    }
+    
+    /**
         Executes block on the background for provided queue.
     
         :param: Name of queue
@@ -56,4 +70,3 @@ public class Dispatch : NSObject {
         }
     }
 }
-
