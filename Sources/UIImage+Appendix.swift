18791a1c035827ae375c0cea5cd70b654cfa4770
Wess Cope <wcope@me.com>
2016-11-08 11:51:32 -0500

    Updates image resizing to mimic old lib more closely

--- Sources/UIImage+Appendix.swift
+++ Sources/UIImage+Appendix.swift
@@ -38,45 +38,126 @@ public  extension UIImage {
 
         return UIImage(CGImage: ref, scale: scale, orientation: .Up)
     }
-
-    public func scale(to size:CGSize, mode:UIImageScale = .aspectFit) -> UIImage? {
-        let ratio = ((size.width / self.size.width), (size.height / self.size.height))
-        let scale:CGFloat 
-
-        switch mode {
-            case .fill:
-            scale = 1
-            break 
-
-            case .aspectFit:
-            scale = min(ratio.0, ratio.1)
-            break 
-
-            case .aspectFill:
-            scale = max(ratio.0, ratio.1)
-            break 
-        }
-
-        let width       = size.width * scale 
-        let height      = size.height * scale 
-        let rect        = CGRect(x: 0.0, y: 0.0, width: width, height: height)
-        let space       = CGColorSpaceCreateDeviceRGB()
-        let bitmap      = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        let transform   = CGAffineTransform.identity
-
-        guard let context = CGContext(data: nil, width: Int(rect.size.width), height: Int(rect.size.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue),
-              let cgImage = self.cgImage else {
-            return self
-        }
-
-        context.concatenate(transform)
-        
-        context.interpolationQuality = CGInterpolationQuality(rawValue: 3)
-
-        context.draw(cgImage, in: rect)
-
-        guard let contextImage = context.makeImage() else { return self}
-
-        return UIImage(cgImage: contextImage, scale: self.scale, orientation: imageOrientation)
-    }    
+  
+  
+  public func resize(to size:CGSize) -> UIImage? {
+    guard let reference = CGImage else { return nil }
+    
+    let width         = CGImageGetWidth(reference)
+    let height        = CGImageGetHeight(reference)
+    let referenceSize = CGSize(width: width, height: height)
+    
+    guard referenceSize == size else { return self }
+    
+    let ratio       = size.width / referenceSize.width
+    var transform   = CGAffineTransformIdentity
+    
+    switch imageOrientation {
+    case .Up:
+      transform = CGAffineTransformIdentity
+      break
+    case .UpMirrored:
+      transform = CGAffineTransformMakeTranslation(referenceSize.width, 0)
+      transform = CGAffineTransformScale(transform, -1, 1)
+      break
+    case .Down:
+      transform = CGAffineTransformMakeTranslation(referenceSize.width, referenceSize.height)
+      transform = CGAffineTransformRotate(transform, CGFloat(M_PI))
+      break
+    case .DownMirrored:
+      transform = CGAffineTransformMakeTranslation(0, referenceSize.height)
+      transform = CGAffineTransformScale(transform, 1, -1)
+      break
+    case .LeftMirrored:
+      let sizeFlip  = CGSize(width: size.height, height: size.width)
+      transform     = CGAffineTransformMakeTranslation(referenceSize.width, referenceSize.height)
+      transform     = CGAffineTransformScale(transform, -1, 0)
+      transform     = CGAffineTransformRotate(transform, CGFloat(3.0 * M_PI_2))
+      break
+    case .Left:
+      let sizeFlip  = CGSize(width: size.height, height: size.width)
+      transform     = CGAffineTransformMakeTranslation(0, referenceSize.width)
+      transform     = CGAffineTransformRotate(transform, CGFloat(3.0 * M_PI_2))
+      break
+    case .RightMirrored:
+      let sizeFlip  = CGSize(width: size.height, height: size.width)
+      transform     = CGAffineTransformMakeScale(-1, 1)
+      transform     = CGAffineTransformRotate(transform, CGFloat(M_PI_2))
+      break
+    case .Right:
+      let sizeFlip  = CGSize(width: size.height, height: size.width)
+      transform     = CGAffineTransformMakeTranslation(referenceSize.height, 0)
+      transform     = CGAffineTransformRotate(transform, CGFloat(M_PI_2))
+      break
+    default:
+      return nil
+    }
+    
+    UIGraphicsBeginImageContextWithOptions(size, false, scale)
+    
+    guard let context = UIGraphicsGetCurrentContext() else { return nil }
+    
+    if imageOrientation == .Right || imageOrientation == .Left {
+      CGContextScaleCTM(context, -ratio, ratio)
+      CGContextTranslateCTM(context, -referenceSize.height, 0)
+    } else {
+      CGContextScaleCTM(context, ratio, -ratio)
+      CGContextTranslateCTM(context, 0, -referenceSize.height)
+    }
+    
+    CGContextConcatCTM(context, transform)
+    
+    CGContextDrawImage(context, CGRect(origin: .zero, size:referenceSize), reference)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
+    
+    return image
+  }
+  
+  public func resizeToFit(in size:CGSize, scale:Bool) -> UIImage? {
+    guard let reference = CGImage else { return nil }
+    
+    var bounding      = size
+    let width         = CGImageGetWidth(reference)
+    let height        = CGImageGetHeight(reference)
+    let referenceSize = CGSize(width: width, height: height)
+    
+    switch imageOrientation {
+    case .Left, .Right, .LeftMirrored, .RightMirrored:
+      bounding = CGSize(width: size.height, height: size.width)
+      break
+      
+    default:
+      break
+    }
+    
+    let toSize:CGSize
+    if !scale && (referenceSize.width < bounding.width) && (referenceSize.height < bounding.height) {
+      toSize = referenceSize
+    } else {
+      let ratio = ((bounding.width / referenceSize.width), (bounding.height / referenceSize.height))
+      let width:CGFloat
+      let height:CGFloat
+      
+      if ratio.0 < ratio.1 {
+        width   = bounding.width
+        height  = CGFloat(floorf(Float(referenceSize.height) * Float(ratio.0)))
+      } else {
+        width   = CGFloat(floorf(Float(referenceSize.width) * Float(ratio.1)))
+        height  = bounding.height
+      }
+      
+      toSize = CGSize(width: width, height: height)
+    }
+    
+    return resize(to: toSize)
+  }
 }
+
+
+
+
+
+
+
