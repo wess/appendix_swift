9c82efe1686dae24d2b5091222589357b071799d
Wess Cope <wcope@me.com>
2017-02-16 12:32:51 -0500

    Adds darker/lighter to color and clean up in UIImage

--- Sources/UIImage+Appendix.swift
+++ Sources/UIImage+Appendix.swift
@@ -14,22 +14,22 @@ public  extension UIImage {
   public func fill(color:UIColor) -> UIImage {
     UIGraphicsBeginImageContextWithOptions(size, false, 0)
     let rect = CGRect(origin: .zero, size: size)
-    
+
     color.set()
     UIRectFill(rect)
-    
+
     draw(in: rect, blendMode: .destinationIn, alpha: 1)
-    
+
     let image = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
-    
+
     return image!
   }
-  
+
   public func crop(to rect:CGRect) -> UIImage {
     let scaled  = CGRect(x: (rect.left * scale), y: (rect.top * scale), width: (rect.width * scale), height:(rect.height * scale))
     let ref     = self.cgImage!.cropping(to: scaled)!
-    
+
     return UIImage(cgImage: ref, scale: scale, orientation: .up)
   }
 }
